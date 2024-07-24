0c4055e0f98af8ff2cac281cbed85dee9d5f7f07
hopeaitchison <hope.aitchison1@gmail.com>
2024-07-24 08:02:27 +0100

    swap memory

--- volume_management.sh
+++ volume_management.sh
@@ -25,6 +25,8 @@ n, 1 (default), empty (default), +1G (any size), p, w
 # create a filesystem
 mkfs.ext4 or mkfs.vfat or mkfs.xfs /dev/device
 
+mkfs.xfs -L myxfs /dev/device # adds a custom label
+
 # mount a fs (specific order)
 mount /dev/device /mnt
 umount /dev/device /mnt
@@ -47,3 +49,33 @@ systemctl daemon-reload # manages mounting
 blkid # prints UUIDs
 blkid | grep /nvme1n1 | awk '{ print $2 }' # extract the UUID
 blkid | grep /nvme1n1 | awk '{ print $2 }' >> /etc/fstab
+lsblk -f | grep "nvme1n1p1" | awk '{ print $3 }'>> /etc/fstab
+
+#######################################
+## SWAP
+# When the physical RAM is fully utilized
+# OS can move some inactive pages of memory to the swap space
+# This process frees up RAM for other active processes
+
+fdisk /dev/nvme1n1
+n   2(default)  empty (default)    +2   P
+m   x (expert)  m   f (fix order)   r (return)  p   w 
+
+lsblk
+fdisk -l /dev/nvme
+
+free -m # check memory, 0 swap
+
+mkswap /dev/nvme1n1p2
+free -m # now 2047
+swapon /dev/nvme1n1p2
+
+blkid | grep nvme1n1p2 | awk '{ print $2 }' >> /etc/fstab
+UUID    none    swap    defaults    0   0
+
+mount -a
+findmnt --verify
+systemctl daemon-reload
+
+# extended partitions use all remaining disk space by default
+# then move on to logical paritions (no. 5 first)
