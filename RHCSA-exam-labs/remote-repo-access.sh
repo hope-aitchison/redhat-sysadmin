992292f2236a9fac6b9a4780f101a0e7ca86ecb9
hopeaitchison <hope.aitchison1@gmail.com>
2024-11-29 14:06:14 +0000

    local-repo

--- RHCSA-exam-labs/remote-repo-access.sh
+++ RHCSA-exam-labs/remote-repo-access.sh
@@ -20,6 +20,30 @@ gpgcheck=0
 
 ### Configuring local repository access
 
+## Make an ISO file of your installation disk and store it as /rhel.iso
+## Mount it persistently on the directory /repo on your local server
+## Configure the local server to access this mounted disk as a repository
+## Verify that you can install packages from this repository
+
+dd if=/dev/sr0 of=rhel9.iso bs=1M # if input file of output file
+mkdir /repo
+vim /etc/fstab
+/rhel9.iso       /repo   iso9660     defaults        0       0
+mount -a
+ls /repo # shows contents of installation disk
+
+dnf config-manager --add-repo file:///repo/BaseOS
+dnf config-manager --add-repo file:///repo/AppStream
+
+cd /etc/yum.repos.d/
+vim repo_BaseOS.repo
+gpgcheck=0 # otherwise installation of anything from this repo not permitted
+
+vim repo_AppStream.repo
+gpgcheck=0 
+
+dnf install -y git # verifies that we can now install packages from 
+
 ### Managing permissions
 
 ### Finding files
