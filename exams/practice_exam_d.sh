4432c98dfe0b2bf103f5e9cddc557e0a544e2e90
hope-aitchison <hope.aitchison1@gmail.com>
2025-03-16 18:11:42 +0000

    most of exam d

--- exams/practice_exam_d.sh
+++ exams/practice_exam_d.sh
@@ -27,16 +27,38 @@ dnf install -y vim
 # Create a 500-MiB partition on your second hard disk, and format it with the Ext4 file system. Mount it persistently on the directory /mydata, 
 # using the label mydata.
 
+mkdir /mydata
 
+fdisk /dev/nvem1n1
+
+lsblk 
+
+mkfs.ext4 -L mydata /dev/nvme1n1p1
+
+vim /etc/fstab
+LABEL=mydata    /mydata     ext4    defaults    0   0
+
+mount -a
+lsblk -f
 
 # Set default values for new users. A user should get a warning three days before expiration of the current password. 
 # Also, new passwords should have a maximum lifetime of 120 days.
 
+vim /etc/login.defs
 
-/etc/security/pwquality
+useradd testuser
+chage -l testuser
 
 #  Create users lori and laura and make them members of the secondary group sales. Ensure that user lori uses UID 2000 and user laura uses UID 2001.
 
+groupadd sales
+
+useradd -u 2000 -G sales lori
+useradd -u 2001 -G sales laura
+
+cat /etc/passwd
+
+cat /etc/passwd | grep sales
 
 # Create shared group directories /groups/sales and /groups/data, and make sure the groups meet the following requirements:
 # * Members of the group sales have full access to their directory.
@@ -45,26 +67,118 @@ dnf install -y vim
 # * Alex is general manager, so user alex has read access to all files in both directories and has permissions to delete 
 # all files that are created in both directories.
 
+mkdir -p /groups/{sales,data}
+
+cd /groups
+
+groupadd data
+
+chown :sales sales
+chown :data data
+
+useradd alex
+
+usermod -aG data,sales alex
+
+chmod 2770 data # means group members have full access to their directory
+chmod 2770 sales
+
 
 #  Create a 1-GiB swap partition and mount it persistently.
 
+lsblk
+
+fdisk /dev/nvme1n1
+
+mkswap /dev/nvme1n1p2
+
+swapon /dev/nvme1n1p2
+
+lsblk -o UUID /dev/nvme1n1p2 >> /etc/fstab
+UUID=xxx    none    swap    defaults    0   0
+
+mount -a
+systemctl daemon-reload
 
 #  Find all files that have the SUID permission set, and write the result to the file /root/suidfiles.
 
+man find # examples
+
+find / -type f -perm -4000 >> /root/suidfiles
 
 # Create a 1-GiB LVM volume group. In this volume group, create a 512-MiB swap volume and mount it persistently.
 
 
-# Add a 10-GiB disk to your virtual machine. On this disk, create a Stratis pool and volume. 
-#  Use the name stratisvol for the volume, and mount it persistently on the directory /stratis.
 
+# Add a 10-GiB disk to your virtual machine. On this disk, create a Stratis pool and volume. 
+# Use the name stratisvol for the volume, and mount it persistently on the directory /stratis.
+
+dnf install -y stratisd
+dnf install -y stratis-cli
+
+lsblk
+
+stratis pool create mypool /dev/nvme1n1
+
+mkdir /stratis
+
+stratis filesystem create mypool stratisvol
+
+stratis filesystem list
+
+lsblk -o UUID /dev/stratis/mypool/stratisvol >> /etc/fstab
+
+UUID=xxx    /stratis    xfs     defaults,x-systemd.requires=stratisd.service    0   0
+
+mount -a
+
+systemctl daemon-reload
 
 
 #  Install an HTTP web server and configure it to listen on port 8080.
 
+dnf install -y httpd
+
+systemctl start httpd
+
+vim /etc/httpd/conf
+
+Listen=8080
+
+cd /var/www/html
+vim index.html
+hello world
+
+man semanage port
+
+semanage port -m -t http_port_t -p tcp 8080
+
+semanage port -l | grep http
+
+systemctl daemon-reload
+systemctl restart httpd
+
+curl localhost:8080
+hello world
+
+journalctl -xe | grep httpd
+ss -tlnp | grep httpd
+
 
 #  Create a configuration that allows user laura to run all administrative commands using sudo.
 
+cd /etc/sudoers.d/
+vim laura-user
+laura ALL=(ALL)  ALL
+
+# or
+
+visudo
+
+# verify
+su - laura
+sudo whoami
+root
 
 #  Create a directory with the name /users and ensure it contains the subdirectories linda and anna. 
 #  Export this directory by using an NFS server.
